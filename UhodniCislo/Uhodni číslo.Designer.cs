a644b2b4005a5d6d26c40630811402775363201c
Ondřej Dudáček <ondrej.dudacek@gmail.com>
2023-04-19 11:36:52 +0200

    Metody do uhodni cislo

---
+++ UhodniCislo/Uhodni číslo.Designer.cs
@@ -0,0 +1,53 @@
+﻿namespace UhodniCislo
+{
+    partial class Uhodni_číslo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Uhodni_číslo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1066, 588);
+            this.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(164)))), ((int)(((byte)(165)))), ((int)(((byte)(169)))));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Uhodni_číslo";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Guess the number";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ UhodniCislo/Uhodni číslo.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UhodniCislo
+{
+    public partial class Uhodni_číslo : Form
+    {
+        private int number;
+        private int length;
+
+        public Uhodni_číslo()
+        {
+            InitializeComponent();
+        }
+        public int GenerateNumber()
+        {
+            Random rnd = new Random();
+            number = rnd.Next(1, 100);
+            return number;
+        }
+
+        public void GenerateClues()
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < 4; i++)
+            {
+                int tmp = rnd.Next(1, 4);
+                switch (tmp)
+                {
+                    case 1:
+                        int biggerThen = 0;
+                        if (number > 25)
+                            biggerThen = 25;
+                        else if (number > 50)
+                            biggerThen = 50;
+                        else if (number > 75)
+                            biggerThen = 75;
+                        Console.WriteLine("The number is bigger then " + biggerThen);
+                        break;
+
+                    case 2:
+                        string evenOdd = "ODD";
+                        if (number % 2 == 0)
+                            evenOdd = "EVEN";
+                        Console.WriteLine("The number is " + evenOdd);
+                        break;
+
+                    case 3:
+                        bool oneInInt = number.ToString().Contains("1");
+                        string oneInString = "NO";
+                        if (oneInInt == true)
+                            oneInString = "YES";
+                        Console.WriteLine("Does the number contains 1: " + oneInString);
+                        break;
+
+                    case 4:
+                        int closeTo = rnd.Next(number + 5, number - 5);
+                        Console.WriteLine("It is close to this number: " + closeTo);
+                        break;
+                }
+            }
+        }
+    }
+}
